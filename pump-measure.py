28842d2bec7688c929e31a616f3d348467241bc5
Leo Dearden <leo@leodearden.org>
2016-04-17 16:51:01 +0100

    Plan script in pseudocode. Extract read_weight method.

--- pump-measure.py
+++ pump-measure.py
@@ -5,6 +5,31 @@ an output file'''
 from datetime import datetime
 import serial, io, re
 
+# parse input flags for data directory and test space parameters
+# build test space tuple list
+# connect to pumps
+# for each test tuple
+#   capture windowed mean weight
+#   calculate expected duration
+#   send command
+#   wait for completion
+#   capture windowed mean weight
+#   record weight difference
+
+def read_weight(sio):
+    while True:
+        try:
+            reading = sio.readline()
+            match = re.search(r'([0-9.]+)g', reading, re.DOTALL)
+            if match:
+                return float(match.group(1))
+            else:
+                print "couldn't parse printer message: '{}'".format(reading)
+        except serial.SerialException as e:
+            #There is no new data from serial port
+            continue
+    
+
 outfile='/Users/leo/data/pump-measure.csv'
 
 ser = serial.Serial(
@@ -17,20 +42,8 @@ sio = io.TextIOWrapper(
 )
 with open(outfile,'a') as f: #appends to existing file
     while ser.isOpen():
-        try:
-            reading = sio.readline()
-        except serial.SerialException as e:
-            #There is no new data from serial port
-            continue
-        except TypeError as e:
-            #Disconnect of USB->UART occured
-            break
-        #\t is tab; \n is line separator
-        match = re.search(r'([0-9.]+)g', reading, re.DOTALL)
-        if match:
-            weight = float(match.group(1))
+        weight = read_weight(sio)
+        if weight:
             f.write(str(weight) + '\n')
             f.flush() #included to force the system to write to disk
-        else:
-            print "couldn't parse printer message: '{}'".format(reading)
 ser.close()
