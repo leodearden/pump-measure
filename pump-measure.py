96b71fd3cef5c3dc13c4533bf077e4a3434f5cb4
Leo Dearden <leo@leodearden.org>
2016-04-17 17:20:48 +0100

    Implement read_mean_weight()

--- pump-measure.py
+++ pump-measure.py
@@ -3,7 +3,7 @@
 an output file'''
 
 from datetime import datetime
-import serial, io, re
+import serial, io, re, numpy
 
 # parse input flags for data directory and test space parameters
 # build test space tuple list
@@ -28,7 +28,15 @@ def read_weight(sio):
         except serial.SerialException as e:
             #There is no new data from serial port
             continue
-    
+
+def read_mean_weight(sio, samples, max_sd_ratio):
+    weights = [read_weight(sio) for _ in xrange(samples)]
+    wa = numpy.array(weights)
+    sd = numpy.std(wa, ddof=1)
+    w = numpy.mean(wa)
+    if sd/w > max_sd_ratio:
+        raise Exception('Excessive deviation in readings. Drift or problem? (readings {}, sd = {}, mean = {})'.format(weights, sd, w)) 
+    return w
 
 outfile='/Users/leo/data/pump-measure.csv'
 
@@ -42,8 +50,5 @@ sio = io.TextIOWrapper(
 )
 with open(outfile,'a') as f: #appends to existing file
     while ser.isOpen():
-        weight = read_weight(sio)
-        if weight:
-            f.write(str(weight) + '\n')
-            f.flush() #included to force the system to write to disk
+        print read_mean_weight(sio, 10, 0.3)
 ser.close()
