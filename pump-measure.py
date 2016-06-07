0ce1d5ef7b7e729399dbff096c9aefa6733db6b6
Leo Dearden <leo@leodearden.org>
2016-06-07 13:47:46 +0100

    Add command line flags for all relevant parameters.

--- pump-measure.py
+++ pump-measure.py
@@ -1,27 +1,11 @@
 #!/usr/bin/env python
-import serial, io, re, numpy, itertools, glob, csv, math, datetime
+import serial, io, re, numpy, itertools, glob, csv, math, datetime, argparse, os
 from printrun.printcore import printcore
 from time import sleep
 from datetime import datetime as dt
+from IPython.utils._tokenize_py2 import String
 
-OUTFILE='/Users/leo/data/pump-measure.{{}}{}.csv'.format(dt.utcnow().isoformat())
-PUMPS = ['Z']
-BROAD_PARAMS = {
-    'n_repeats': 10,
-    'max_duration': 61,
-    'revs': (1, 3, 10, 30, 100, 300, 600),
-    'rates': (1, 3, 10, 30, 100, 300, 1000, 1800, 3000),
-    'pumps': PUMPS
-}
-DEEP_PARAMS = {
-    'n_repeats': 150,
-    'max_duration': 36000,
-    'revs': (0.1, 0.3, 1),
-    'rates': (10, 100, 3000),
-    'pumps': PUMPS
-}
-WAIT_S = 2.5
-START_WEIGHT = 100
+OUTFILE='pump-measure.{{}}{}.csv'.format(dt.utcnow().isoformat())
 
 def read_all(sio, ser):
     read = []
@@ -73,18 +57,20 @@ def set_to_weight(sio, ser, pump, target, eps=0.1):
     while math.fabs(error) > eps:
         print "set_to_weight: error = " + str(error)
         revs = - error / MASS_PER_REV
-        printer.send('G0 {}{:} F{}'.format(pump, revs, RATE))
-        sleep(60 * revs / RATE + WAIT_S)
+        printer.send('G0 {}{} F{}'.format(pump, revs, RATE))
+        sleep(60 * revs / RATE + args.wait)
         error = read_weight(sio, ser) - target
 
 class Test(object):
-    def __init__(self, rate, revs, pump, repeats, writer_container, result_file):
+    def __init__(self, rate, revs, pump, repeats, writer_container, result_file, wait_s, initial_mass):
         self.rate = rate
         self.revs = revs
         self.pump = pump
         self.repeats = repeats
         self.writer_container = writer_container
         self.result_file = result_file
+        self.wait_s = wait_s
+        self.initial_mass = initial_mass
         self.result = None
         self.default_result = {
             'pump':self.pump, 
@@ -126,15 +112,15 @@ class Test(object):
 
     def run(self, sio, ser):
         self.result = dict(self.default_result)
-        set_to_weight(sio, ser, self.pump, START_WEIGHT)
+        set_to_weight(sio, ser, self.pump, self.initial_mass)
         for rep in xrange(self.repeats):
             self.result['time'] = dt.utcnow().isoformat()
             start_weight = read_weight(sio, ser)
-            for command, name in ((t.forward, 'F'), (t.back, 'R')):
+            for command, name in ((self.forward, 'F'), (self.back, 'R')):
                 drain(sio, ser)
                 print 'sending "{}"'.format(command)
                 printer.send(command)
-                sleep(t.duration + WAIT_S)
+                sleep(self.duration + self.wait_s)
                 mn, mx = read_weights(sio, ser)
                 delta = mx - mn
                 print 'mn = {}, mx = {}, delta = {}'.format(mn, mx, delta)
@@ -146,18 +132,100 @@ class Test(object):
         self.result_writer.writerow(self.result)
         self.result_file.flush()
 
-
-def generate_tests(n_repeats, max_duration, revs, rates, pumps, result_file):
+def generate_tests(n_repeats, max_duration, revs, rates, pumps, result_file, args):
     writer_container = []
     tests = [
-        Test(rate, revs, p, n_repeats, writer_container, result_file)
+        Test(
+            rate=rate,
+            revs=revs,
+            pump=p,
+            repeats=n_repeats,
+            writer_container=writer_container,
+            result_file=result_file,
+            wait_s=args.wait,
+            initial_mass=args.initial_mass)
         for rate, revs, p in itertools.product(rates, revs, pumps)
     ]
     return filter(lambda t: t.duration <= max_duration, tests)
 
+parser = argparse.ArgumentParser(description='Test a pump. Write the results in CSV format to one or more files.')
+parser.add_argument(
+    '--pump', '-p',
+    help='Test this pump',
+    choices=['X', 'Y', 'Z'],
+    default='Z')
+parser.add_argument(
+    '--result-path', '-r',
+    help='Write the result files to this directory. The directory must exist.',
+    default='/tmp')
+parser.add_argument(
+    '--deep', '-d',
+    help='Run the deep test suite: Get the specified number of samples for each of a small range of small volumes.',
+    type=int,
+    default=0)
+parser.add_argument(
+    '--broad', '-b',
+    help='Run the broad test suite: Get the specified number of samples for each of a large range of volumes and rates.',
+    type=int,
+    default=10)
+parser.add_argument(
+    '--short', '-s',
+    help='Run short versions of any requested suites, with more limited combinations of parameters.',
+    action='store_true')
+parser.add_argument(
+    '--top-pan-balance', '-t',
+    help='Read fluid mass measurements from a top pan balance connected to this serial port.',
+    default='/dev/tty.usbserial')
+parser.add_argument(
+    '--controller', '-c',
+    help='Send pump move commands to a G-code interpreter connected to this serial port. This can be a file glob.',
+    default='/dev/tty.usbmodem*')
+parser.add_argument(
+    '--wait', '-w',
+    help='Wait this many seconds after the end of the pumping operation before taking a mass measurement (to allow the top pan balance to stabilise). Can be any decimal.',
+    type=float,
+    default=2.5)
+parser.add_argument(
+    '--initial-mass', '-m',
+    help='Pump this mass of material (decimal g) on to the top pan balance before starting each test (ie: before beginning to take samples with any given combination of pump revolutions and rate).',
+    type=float,
+    default=100)
+args = parser.parse_args()
+
+if args.short:
+    broad_params = {
+        'n_repeats': args.broad,
+        'max_duration': 21,
+        'revs': (1, 10, 100),
+        'rates': (10, 100, 1000, 3000),
+        'pumps': [args.pump]
+    }
+    deep_params = {
+        'n_repeats': args.deep,
+        'max_duration': 3600,
+        'revs': (0.3, 1),
+        'rates': (100, 3000),
+        'pumps': [args.pump]
+    }
+else:
+    broad_params = {
+        'n_repeats': args.broad,
+        'max_duration': 61,
+        'revs': (1, 3, 10, 30, 100, 300, 600),
+        'rates': (1, 3, 10, 30, 100, 300, 1000, 1800, 3000),
+        'pumps': [args.pump]
+    }
+    deep_params = {
+        'n_repeats': args.deep,
+        'max_duration': 36000,
+        'revs': (0.1, 0.3, 1),
+        'rates': (10, 100, 3000),
+        'pumps': [args.pump]
+    }
+
 print 'opening serial port...'
 with serial.Serial(
-    port='/dev/tty.usbserial',
+    port=args.top_pan_balance,
     baudrate=9600,
 ) as ser:
     print 'done.'
@@ -168,8 +236,9 @@ with serial.Serial(
         errors='backslashreplace'
     ) as sio:
         print 'done.'
-        usb_modem_names = glob.glob('/dev/tty.usbmodem*')
-        assert len(usb_modem_names) == 1, "Too many usb modems instantiated. Can't tell which one is the Smoothieboard."
+        usb_modem_names = glob.glob(args.controller)
+        assert len(usb_modem_names) > 0, "No Smoothieboard found. Try '--controller=<controller_port>'?"
+        assert len(usb_modem_names) == 1, "More than one file matches. Can't tell which one is the Smoothieboard."
         printer_interface = usb_modem_names[0]
         print 'opening printer_interface = {} ...'.format(printer_interface)
         printer = printcore()
@@ -179,18 +248,17 @@ with serial.Serial(
         print 'configuring Smoothie board...'
         printer.send("G91")
         print 'done.'
-        try:
-            for test_set_name, test_set_params in (('deep', DEEP_PARAMS), ('broad', BROAD_PARAMS)):
-                with open(OUTFILE.format(test_set_name),'w') as result_file:
-                    print 'generating tests...'
-                    tests = generate_tests(result_file=result_file, **test_set_params)
-                    runtime = sum([2 * t.repeats * (t.duration + WAIT_S) for t in tests])
+        for test_set_name, test_set_params in (('deep', deep_params), ('broad', broad_params)):
+            result_file_name = os.path.join(args.result_path, OUTFILE.format(test_set_name))
+            if test_set_params['n_repeats']:
+                with open(result_file_name,'w') as result_file:
+                    print 'generating {} tests...'.format(test_set_name)
+                    tests = generate_tests(args=args, result_file=result_file, **test_set_params)
+                    runtime = sum([2 * t.repeats * (t.duration + t.wait_s) for t in tests])
                     print 'done.'
                     print 'starting {} tests (expected runtime {})...'.format(test_set_name, datetime.timedelta(seconds=runtime))
                     for t in tests:
                         t.run(sio, ser)
                         print t
                     print 'done.'
-        except Exception as e:
-            print 'Exiting on error: ' + str(e)
         printer.disconnect()
