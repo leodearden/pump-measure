7a4f8eb0b0b3aa9d7ff559c82363f9f7207fa550
Leo Dearden <leo@leodearden.org>
2016-06-21 17:44:00 +0100

    Plot forward and reverse time serieses on the same graph (but in different colours). Histograms probably broken.

--- analyse/deep.py
+++ analyse/deep.py
@@ -44,38 +44,48 @@ all_revs = sorted(set([rev for rev, _ in data.iterkeys()]))
 all_rates = sorted(set([rate for _, rate in data.iterkeys()]))
 
 measurement_names = [name for _, name in measurements]
+colours = ['r', 'g', 'b']
 
 figs = {}
 axiess = {}
-for name in measurement_names:
-    figs[name], axiess[name] = plt.subplots(nrows=len(all_revs),
-                                            ncols=len(all_rates),
-                                            sharex=True)
-    figs[name].suptitle(name)
-    for i, revs in enumerate(all_revs, 0):
-        all_relevant_data = sum(
-            [
-                data.get((revs, rate), {name:[]})[name]
-                for rate in all_rates], 
-            [])
-        min_datum = min(
-            all_relevant_data)
-        max_datum = max(
-            all_relevant_data)
-        for j, rate in enumerate(all_rates, 0):
-            print 'looking for ({}, {})'.format(revs, rate)
-            axis = axiess[name][i][j]
-            if (revs, rate) in data:
-                test_results = numpy.array(data[revs, rate][name])
-                title = '{}, {}'.format(str(revs), str(rate))
-                print "charting " + title
-                axis.set_ylim(min_datum, max_datum)
-                if args.histogram:
-                    axis.hist(test_results, normed=True)
-                if args.time_series:
-                    axis.plot(test_results)
-                axis.set_title(title)
-            else:
-                axis.set_visible(False)
+figs[name], axiess[name] = plt.subplots(nrows=len(all_revs),
+                                        ncols=len(all_rates),
+                                        sharex=True)
+figs[name].suptitle(name)
+for i, revs in enumerate(all_revs, 0):
+    all_relevant_data = sum(
+        [
+            data.get((revs, rate), {name:[]})[name]
+            for rate in all_rates],
+        [])
+    min_datum = min(
+        all_relevant_data)
+    max_datum = max(
+        all_relevant_data)
+    for j, rate in enumerate(all_rates, 0):
+        print 'looking for ({}, {})'.format(revs, rate)
+        axis = axiess[name][i][j]
+        if (revs, rate) in data:
+            plot_params = zip(
+                numpy.array([
+                    range(len(data[revs, rate][name]))
+                    for name in measurement_names
+                ]),
+                numpy.array([
+                    data[revs, rate][name]
+                    for name in measurement_names
+                ]),
+                colours)
+            title = '{}, {}'.format(str(revs), str(rate))
+            print "charting " + title
+            axis.set_ylim(min_datum, max_datum)
+            if args.histogram:
+                axis.hist(*plot_params, normed=True)
+            if args.time_series:
+                for x, y, format in plot_params:
+                    axis.plot(x, y, format)
+            axis.set_title(title)
+        else:
+            axis.set_visible(False)
 
 plt.show()
